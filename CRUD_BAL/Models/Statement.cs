0d3ed58523227a1b500571bbfb061f346dc04d2b
kogo <30936224+saba161@users.noreply.github.com>
2021-06-14 02:43:16 -0700

    Delete CRUD_BAL directory

--- CRUD_BAL/Models/Statement.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CRUD_BAL.Models
-{
-    public class Statement
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public byte Photo { get; set; }
-    }
-}
